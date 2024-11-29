1716fe36f11fcca7bffb2fc1891ca5e9816bec97
Er Ankur Gupta <ankurdotcom@users.noreply.github.com>
2024-11-29 14:39:43 +0000

    WIP

--- src/main/java/com/example/tcs/TCS.java
+++ src/main/java/com/example/tcs/TCS.java
@@ -7,7 +7,7 @@ public class TCS {
 
 
     public static void main(String arg []) {
-        HashMap<String,Integer> books = new HashMap<>();
+        Map<String,Integer> books = new HashMap<>();
         books.put("Java",500);
         books.put("HTML",300);
         books.put("CSS",200);
@@ -19,13 +19,11 @@ public class TCS {
         //Task is to sort this hashmap based on value
 
         //Convert map to stream
-        HashMap<String,Integer> sortedBook = books.entrySet().stream().compare((e1,e2) -> e1.value().compare(e2.value())).collect(Collectors.toMap());
+        // HashMap<String,Integer> sortedBook = books.entrySet().stream().compare((e1,e2) -> e1.value().compare(e2.value())).collect(Collectors.toMap());
         
-        System.out.println(sortedBook);
+        //System.out.println(sortedBook);
         //Apply Intermediate comparator Based on Price
         //terminal Operation to collection new Values
-        
-        System.out.println(books);
     }
 }
 
